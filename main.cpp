bad0487380d91098a4be7c06064bfc22ee45f3e8
JBank25 <urbankjoshua1@gmail.com>
2025-06-09 15:28:32 -0500

    Toggle calendar-ish

--- main.cpp
+++ main.cpp
@@ -1,3 +1,12 @@
+#include <memory> // for allocator, __shared_ptr_access
+#include <string> // for string, basic_string
+#include <vector> // for vector
+
+#include "ftxui/component/captured_mouse.hpp" // for ftxui
+#include "ftxui/component/component.hpp"      // for Toggle, Renderer, Vertical
+#include "ftxui/component/component_base.hpp" // for ComponentBase
+#include "ftxui/component/screen_interactive.hpp" // for Component, ScreenInteractive
+#include "ftxui/dom/elements.hpp" // for text, hbox, vbox, Element
 #include <cstdio>
 #include <cstdlib>
 #include <ctime>
@@ -34,7 +43,9 @@ std::vector<std::vector<std::string>> generateStringWeekNums(int startNum) {
   // Sunday Monday Tuesday Wednesday Thursday Friday Saturday
   //                  1         2       3       4       5
   for (; counter < startNum; counter++) {
-    curResult.push_back("     ");
+    std::ostringstream oss;
+    oss << std::setfill(' ') << std::setw(3) << "";
+    curResult.push_back(oss.str());
   }
   for (int i = counter; i <= 31; i++) {
     if (curResult.size() % 7 == 0 && !curResult.empty()) {
@@ -59,25 +70,32 @@ int main() {
       generateStringWeekNums(zellerDayOfWeek(2025, 7, 1));
 
   auto tableDays = Table({{daysOfWeek}});
+  int week1Selected = 0;
+  int week2Selected = 0;
+  int week3Selected = 0;
+  int week4Selected = 0;
 
-  tableDays.SelectAll().Border(LIGHT);
-  tableDays.SelectRow(1).Border(DOUBLE);
-  tableDays.SelectRow(1).SeparatorHorizontal(LIGHT);
+  Component toggleWeek1 = Toggle(&dayNums.at(0), &week1Selected);
+  Component toggleWeek2 = Toggle(&dayNums.at(1), &week2Selected);
+  Component toggleWeek3 = Toggle(&dayNums.at(2), &week3Selected);
+  Component toggleWeek4 = Toggle(&dayNums.at(3), &week4Selected);
 
-  auto tableNums = Table({{dayNums}});
+  auto container = Container::Vertical({
+      toggleWeek1,
+      toggleWeek2,
+      toggleWeek3,
+      toggleWeek4,
+  });
 
-  tableNums.SelectAll().Border(LIGHT);
-  tableNums.SelectRow(1).Border(DOUBLE);
-  tableNums.SelectRow(1).SeparatorHorizontal(LIGHT);
-
-  auto documentDays = tableDays.Render();
-  auto documentNums = tableNums.Render();
-  auto screenDays =
-      Screen::Create(Dimension::Full(), Dimension::Fit(documentDays));
-  auto screenNums =
-      Screen::Create(Dimension::Full(), Dimension::Fit(documentNums));
-  Render(screenDays, documentDays);
-  Render(screenNums, documentNums);
-  screenDays.Print();
-  screenNums.Print();
+  auto renderer = Renderer(container, [&] {
+    return vbox({
+        filler(),
+        hbox(toggleWeek1->Render()),
+        hbox(toggleWeek2->Render()),
+        hbox(toggleWeek3->Render()),
+        hbox(toggleWeek4->Render()),
+    });
+  });
+  auto screen = ScreenInteractive::TerminalOutput();
+  screen.Loop(renderer);
 }
